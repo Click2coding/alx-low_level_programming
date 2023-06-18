071ffd5d78ec3dff85a881a51e88e82ed3907920
click2coding <assefaendale7@gmail.com>
2023-06-18 13:10:57 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all possible combinations of single
+ * numbers from 0 - 9 using putchar()
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
