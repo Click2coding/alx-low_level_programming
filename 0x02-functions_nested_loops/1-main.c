b1b00b217119e57638760ef7619c4b69d61cfa6b
click2coding <assefaendale7@gmail.com>
2023-06-23 16:05:58 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -3,10 +3,11 @@
 /**
  * main - check the code
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    print_alphabet();
-    return (0);
+	print_alphabet();
+	return (0);
 }
