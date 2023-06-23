3d903cd20f5c16952a505cc67d68b082c726109f
click2coding <assefaendale7@gmail.com>
2023-06-23 16:32:57 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,19 @@
 
 /**
  * print_alphabet - entry point
- * Return: void
+ *
+ * Decription: Prints alphabet in lowercase, followed by new line
+ *
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
 {
-	char c;
+	char alphabet;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-	_putchar(c);
+		_putchar(alphabet);
 	}
 
 	_putchar('\n');
